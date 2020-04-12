52f21a419ffe3e16c5dea0efa96aca378e8bb490
David-Alexandre <DocKob@users.noreply.github.com>
2020-04-12 18:11:51 +0200

    Add Notify Function

--- Private/System.ps1
+++ Private/System.ps1
@@ -147,3 +147,47 @@ function Set-HtAccessibility {
     Set-Service WinRM -StartupType Automatic
 
 }
+
+function Show-HtNotification {
+    [CmdletBinding(
+        SupportsShouldProcess = $true
+    )]
+    Param(
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Info", "Warning", "Error", "None")]
+        [string]$Type,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Title,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Text,
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [int]$Timeout = 10
+    )      
+            
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    $notify = new-object system.windows.forms.notifyicon
+    $notify.icon = [system.drawing.icon]::ExtractAssociatedIcon((join-path $pshome powershell.exe))
+    $notify.visible = $True
+
+    $notify.showballoontip($Timeout, $title, $text, $type)
+
+    switch ($Host.Runspace.ApartmentState) {
+        STA {
+            $null = Register-ObjectEvent -InputObject $notify -EventName BalloonTipClosed -Action {
+                $Sender.Dispose()
+                Unregister-Event $EventSubscriber.SourceIdentifier
+                Remove-Job $EventSubscriber.Action
+            }
+        }
+        default {
+            continue
+        }
+    }
+
+}  
